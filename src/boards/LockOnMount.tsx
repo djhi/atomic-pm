f99edeebe051e321c06a08d25e870b23f281d762
Gildas <1122076+djhi@users.noreply.github.com>
2025-02-25 13:30:28 +0100

    Add missing migration for locks & fix locks UI

--- src/boards/LockOnMount.tsx
+++ src/boards/LockOnMount.tsx
@@ -7,25 +7,24 @@ export const LockOnMount = () => {
   const { data: lock } = useGetLockLive();
   const { data: editor } = useGetOne(
     "profiles",
-    {
-      id: lock?.identity,
-    },
-    {
-      enabled: !!lock,
-    },
+    { id: lock?.identity },
+    { enabled: !!lock },
   );
+  if (!error && !isLocked) {
+    return null;
+  }
   return (
     <Box>
-      {error && (
+      {error ? (
         <Alert severity="info" sx={{ borderRadius: 0 }}>
           <AlertTitle>Post is readonly</AlertTitle> Edited by {editor?.email}.
         </Alert>
-      )}
-      {isLocked && (
+      ) : null}
+      {isLocked ? (
         <Alert severity="success" sx={{ borderRadius: 0 }}>
           <AlertTitle>Post locked</AlertTitle> Only you can edit it.
         </Alert>
-      )}
+      ) : null}
     </Box>
   );
 };
