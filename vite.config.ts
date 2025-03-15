0b98a705e06db5f3fc9d6e52b841120796869a81
Gildas <1122076+djhi@users.noreply.github.com>
2025-03-15 10:42:39 +0100

    Fix vite env variable

--- vite.config.ts
+++ vite.config.ts
@@ -35,7 +35,7 @@ const alias = [
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  define: { 'process.env.NODE_ENV': mode },
+  define: { 'process.env.NODE_ENV': `"${mode}"` },
   server: {
     host: true,
   },
