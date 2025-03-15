cd6e92bb29a0fe48b249f6243c0114b7bc621787
Gildas <1122076+djhi@users.noreply.github.com>
2025-03-15 10:40:23 +0100

    include NODE_ENV

--- vite.config.ts
+++ vite.config.ts
@@ -35,6 +35,7 @@ const alias = [
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
+  define: { 'process.env.NODE_ENV': mode },
   server: {
     host: true,
   },
